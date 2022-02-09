f4084fdab6e6ddf6d318db159841edcfc5b2aa45
Santiago Donoso <sand@kea.dk>
2022-02-09 09:35:25 +0100

    ...

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-from bottle import get, run
+from bottle import get, post, request, response, run
+import json
+import uuid
+
+items = [
+  {"id":"5cee299b-6630-4aec-a7cc-f69947a64908", "name":"a"}
+]
+
 
 ##############################
 @get("/")
@@ -8,10 +15,19 @@ def _():
 ##############################
 @get("/items")
 def _():
-  return "items"
+  response.content_type = "application/json"
+  return json.dumps(items)
 
 
 ##############################
+@post("/items")
+def _():
+  item_id = str(uuid.uuid4())
+  item_name = request.json.get("name")
+  print( type(item_id) )
+  return item_name
+
+
 
 ##############################
 # 65535 
