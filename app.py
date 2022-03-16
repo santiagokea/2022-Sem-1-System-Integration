ecf144199d22fadf6c24067b3f4705f822468e07
Santiago Donoso <sand@kea.dk>
2022-03-16 09:31:28 +0100

    ...

--- app.py
+++ app.py
@@ -7,23 +7,30 @@ import sqlite3
 # print(fake.first_name()) # Ronald
 # print(fake.email()) # nancygarner@example.org
 
-users = [
-  {
+# users = []
+# for _ in range(10):
+#   user = {
+#     "user_id" : str(uuid.uuid4()),
+#     "user_name" : fake.first_name(),
+#     "user_email" : fake.email()
+#   }
+#   users.append(user)
+
+users = [  {
     "user_id" : str(uuid.uuid4()),
     "user_name" : fake.first_name(),
-    "user_email" : fake.email()
-  },
-  {
-    "user_id" : str(uuid.uuid4()),
-    "user_name" : fake.first_name(),
-    "user_email" : fake.email()
-  }, 
-]
+    "user_email" : fake.email(),
+    "user_password" : fake.password(),
+  } for _ in range(10000)  ]
+
 
 try:
   db = sqlite3.connect("database.db")
-  counter = db.executemany("INSERT INTO users VALUES(:user_id, :user_name, :user_email)", users).rowcount
+  counter = db.executemany("""
+    INSERT INTO users 
+    VALUES(:user_id, :user_name, :user_email, :user_password)""", users).rowcount
   if not counter: print("uppps...") 
+  print(f"Rows added: {counter}")
   db.commit()
 except Exception as ex:
   print(ex)  
