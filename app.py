ae1a632c7e7c8450c0f4041195af2750dc4a6bea
Santiago Donoso <sand@kea.dk>
2022-03-16 09:13:32 +0100

    ...

--- app.py
+++ app.py
@@ -7,15 +7,22 @@ import sqlite3
 # print(fake.first_name()) # Ronald
 # print(fake.email()) # nancygarner@example.org
 
-user = {
-  "user_id" : str(uuid.uuid4()),
-  "user_name" : fake.first_name(),
-  "user_email" : fake.email()
-}
+users = [
+  {
+    "user_id" : str(uuid.uuid4()),
+    "user_name" : fake.first_name(),
+    "user_email" : fake.email()
+  },
+  {
+    "user_id" : str(uuid.uuid4()),
+    "user_name" : fake.first_name(),
+    "user_email" : fake.email()
+  }, 
+]
 
 try:
   db = sqlite3.connect("database.db")
-  counter = db.execute("INSERT INTO users VALUES(:user_id, :user_name, :user_email)", user).rowcount
+  counter = db.executemany("INSERT INTO users VALUES(:user_id, :user_name, :user_email)", users).rowcount
   if not counter: print("uppps...") 
   db.commit()
 except Exception as ex:
